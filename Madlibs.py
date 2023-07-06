1e944bd60c48a0c10b51dafeb94eb24c394197f4
unknown <beingmkulkarni07@gmail.com>
2023-07-07 00:20:52 +0530

    1st commit

---
+++ Madlibs.py
@@ -0,0 +1,8 @@
+Name=input("enter a name :").capitalize()
+Place=input("Place :").capitalize()
+verb1=input("Action verb1:")
+verb2=input("Action verb2:")
+person=input("Famous Person:")
+
+
+print(f"{Name} is going to visit {Place}. {verb1} and {verb2} with the celebrit {person}")
